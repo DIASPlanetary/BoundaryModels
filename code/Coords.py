08ebce3cbffd4756c89bc553248c293e36619387
mjrutala <mrutala@bu.edu>
2024-06-11 10:47:19 +0100

    Initial commit: framework for calling boundary models

---
+++ code/Coords.py
@@ -0,0 +1,53 @@
+def Coords(planet, boundary, x=False, y=False, z=False, p_dyn=False, model=''):
+    """
+    
+    
+    Parameters
+    ----------
+    planet : TYPE
+        DESCRIPTION.
+    boundary : TYPE
+        DESCRIPTION.
+    x : TYPE
+        DESCRIPTION.
+    y : TYPE
+        DESCRIPTION.
+    z : TYPE
+        DESCRIPTION.
+    model : TYPE, optional
+        DESCRIPTION. The default is model.
+
+    Returns
+    -------
+    None.
+
+    """
+    import logging
+    
+    import Jupiter_Coords
+    
+    logger = logging.getLogger(__name__)
+    
+    #   Check which planet we're looking at
+    #   I'd much rather use match/case, but Spyder/PyFlakes linting is still...
+    #   busted
+    if planet.lower() in ['mercury']:
+        #
+        pass
+    elif planet.lower() in ['earth']:
+        #
+        pass
+    elif planet.lower() in ['mars']:
+        #
+        pass
+    elif planet.lower() in ['jupiter']:
+
+        #
+        result = Jupiter_Coords(boundary, x, y, z, model='')
+    elif planet.lower() in ['saturn']:
+        #
+        pass
+    else:
+        msg = '{1} is not a recognized/supported! Returning...'
+        logger.warning(msg.format(planet))
+        return
